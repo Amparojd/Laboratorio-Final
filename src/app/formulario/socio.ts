0486dc1a0e35ffb82650b93d44691f050a70186d
Amparo Jiménez <99424503+Amparojd@users.noreply.github.com>
2022-06-10 17:54:12 +0200

    Add files via upload

---
+++ src/app/formulario/socio.ts
@@ -0,0 +1,20 @@
+export class Socio {
+  nSocio:number;
+  nombre: string;
+  apellido:string;
+  dni:string;
+  telefono:number;
+  sexo:string;
+
+
+
+  constructor() {
+    this.nSocio=0;
+    this.nombre="";
+    this.apellido="";
+    this.dni="";
+    this.telefono=0;
+    this.sexo="";
+  }
+
+}
